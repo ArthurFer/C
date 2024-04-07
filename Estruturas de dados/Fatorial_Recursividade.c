30e49cba4ddf514bef1f7fa7e813d5b772c5b131
Arthur Fernando <arthurfernando18@gmail.com>
2024-04-06 23:49:18 -0300

    Melhoria no fatorial recursivo

--- Estruturas de dados/Fatorial_Recursividade.c
+++ Estruturas de dados/Fatorial_Recursividade.c
@@ -11,6 +11,12 @@ int main(){
     int n;
     printf("Fatorial de: ");
     scanf("%d", &n);
-    printf("Fatorial de %d = %d\n", n, fatorial(n));
+
+    for (int i = 0; i < n; i++){
+        int result = fatorial(i);
+        int controle = i +1;
+        printf("Fatorial de %d! = %d x %d = %d\n", i +1, i+1, result, controle * result);        
+        }
+        
     return 0;
 }
